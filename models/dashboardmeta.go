9e340b55ca390350a8b601b72c234c563d100576
F2634446 Edilson Alves de Azevedo Junior <uere@bb.com.br>
2022-05-25 18:15:26 -0300

    ja fazendo download

--- models/dashboardmeta.go
+++ models/dashboardmeta.go
@@ -7,6 +7,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/uere/grafana-backup/utils"
 )
@@ -65,30 +66,36 @@ func ListDashboards(b *Backup) []DashboardMeta {
 }
 
 func GetDashboards(b *Backup, d []DashboardMeta) {
-	req, _ := http.NewRequest("GET", b.Url+"/api/dashboards/"+d[1].Uri, nil)
-	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
-	autorizacao := base64.StdEncoding.EncodeToString([]byte(b.Login + ":" + b.Password))
-	req.Header.Add("Authorization", "Basic "+autorizacao)
-	req.Header.Add("Content-Type", "application/json")
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Println("Error on response.\n[ERROR] -", err)
-	}
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Println("Error while reading the response bytes:", err)
-	}
-	// fmt.Println(string([]byte(body)))
-	if err := utils.MakeDir("dashboards"); err != nil {
-		log.Println("Error on create directory", err)
-	}
-	if err := utils.CreateFile("dashboards/" + d[1].Title + ".json"); err != nil {
-		log.Println("Error on create directory", err)
+
+	for _, v := range d {
+		nomearquivo := strings.Split(v.Uri, "/")
+		req, _ := http.NewRequest("GET", b.Url+"/api/dashboards/"+v.Uri, nil)
+		http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+		autorizacao := base64.StdEncoding.EncodeToString([]byte(b.Login + ":" + b.Password))
+		req.Header.Add("Authorization", "Basic "+autorizacao)
+		req.Header.Add("Content-Type", "application/json")
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			log.Println("Error on response.\n[ERROR] -", err)
+		}
+		defer resp.Body.Close()
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Println("Error while reading the response bytes:", err)
+		}
+		// fmt.Println(string([]byte(body)))
+		if err := utils.MakeDir("dashboards"); err != nil {
+			log.Println("Error on create directory", err)
+		}
+		if err := utils.CreateFile("dashboards/" + nomearquivo[1] + ".json"); err != nil {
+			log.Println("Error on create directory", err)
+		}
+
+		if err = ioutil.WriteFile("dashboards/"+nomearquivo[1]+".json", []byte(body), 0777); err != nil {
+			log.Println("Error on save File.\n[ERROR] -", err)
+		}
+
 	}
 
-	if err = ioutil.WriteFile("dashboards/"+d[1].Title+".json", []byte(body), 0777); err != nil {
-		log.Println("Error on save File.\n[ERROR] -", err)
-	}
 }
